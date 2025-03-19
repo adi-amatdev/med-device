2569f1af4a639e8d7e9ac3fb91ca46ef99b6ae36
aadi <1rn20cs169.udupiaadityaacharya@gmail.com>
2025-03-19 14:27:47 +0530

    added file req body option

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-
+const multer = require('multer')
 const app = express();
 app.use(express.json());
 
 // Middleware to handle raw binary file upload
+const upload = multer({
+    limits: { fileSize: 1 * 1024 * 1024 }, // 1MB
+    storage: multer.diskStorage({
+        destination: path.join(__dirname, 'uploads'),
+        filename: (req, file, cb) => {
+            cb(null, `${Date.now()}-${file.originalname}`);
+        }
+    })
+});
+
+app.post('/upload-file-direct', upload.single('file'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded or file size exceeds limit." });
+    }
+
+    return res.status(200).json({ message: "File uploaded successfully", filename: req.file.originalname });
+});
+
 app.post('/upload', (req, res) => {
     const uploadDir = path.join(__dirname, 'uploads');
 
